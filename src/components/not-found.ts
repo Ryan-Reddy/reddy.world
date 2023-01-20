e8febc705e01a4ea5c8843b295fa28fe6c7c70ed
Ryan Reddy <90551370+klugook@users.noreply.github.com>
2023-01-20 02:31:42 +0100

    add not-found.ts component as 404

---
+++ src/components/not-found.ts
@@ -0,0 +1,118 @@
+import { css, html, LitElement } from 'lit';
+import { customElement, property } from 'lit-element';
+
+/**
+ * An example element.
+ *
+ * @slot - This element has a slot
+ * @csspart button - The button
+ */
+@customElement('support-element')
+export class SupportElement extends LitElement {
+  @property() _hiddenElement = 'hidden';
+  constructor() {
+    super();
+  }
+
+  static get styles() {
+    return css`
+      * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+        text-decoration: none;
+        line-height: 1.2;
+
+      }
+
+      .hidden {
+        display: none;
+        pointer-events: none;
+        color: lightgray;
+        foreground-color: var(--kpn-grijs);
+        background-color: var(--kpn-grijs);
+      }
+      button {
+        width: 66%;
+        height: 2em;
+      }
+      html {
+        color: #888;
+        display: table;
+        font-family: sans-serif;
+        height: 100%;
+        text-align: center;
+        width: 100%;
+      }
+
+      body {
+        display: table-cell;
+        vertical-align: middle;
+        margin: 2em auto;
+      }
+
+      h1 {
+        color: #555;
+        font-size: 2em;
+        font-weight: 400;
+      }
+
+      p {
+        margin: 0 auto;
+        width: 280px;
+      }
+
+      @media only screen and (max-width: 280px) {
+
+        body,
+        p {
+          width: 95%;
+        }
+
+        h1 {
+          font-size: 1.5em;
+          margin: 0 0 0.3em;
+        }
+
+      }
+    `;
+  }
+
+  render() {
+    return html`<!doctype html>
+    <html lang="en">
+
+    <head>
+      <meta charset="utf-8">
+      <title>Page Not Found</title>
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    </head>
+
+    <body>
+    <h1>Page Not Found</h1>
+
+    <p>Sorry, but the page you were trying to view does not exist.
+      But sometimes that's when you find something you didn't know you where looking for.
+    </p>
+    <br>
+    <audio id="my_audio" src="Yosemitebear_Mountain_Double_Rainbow_1-8-10.mp3" loop="loop"></audio>
+    <img src="../public/double_rainbow.png" height="533" width="800" alt="double rainbow all the way meme beautiful"/>
+
+    </body>
+    <script>
+      window.onload = function () {
+        document.getElementById("my_audio").play();
+      }
+    </script>
+
+    </html>
+    <!-- IE needs 512+ bytes: https://docs.microsoft.com/archive/blogs/ieinternals/friendly-http-error-pages -->
+    `;
+  }
+  _clickForSupport() {
+    console.log('Click for support');
+    console.log(this._hiddenElement);
+    this._hiddenElement = this._hiddenElement == 'hidden' ? '' : 'hidden';
+  }
+}
--- src/vaadin-routing.js
+++ src/vaadin-routing.js
@@ -14,10 +14,24 @@ router.setRoutes([
     path: '/login',
     component: 'login-element',
   },
-  // {
-  //   name: 'register',
-  //   path: '/register',
-  //   component: 'register-form',
-  // },
-  // etc...
+  {
+    name: 'login-element', // doet niks impliciet tenzij je het gebruikt
+    path: '/login',
+    component: 'login-element',
+  },
+  {
+    name: 'login-element', // doet niks impliciet tenzij je het gebruikt
+    path: '/login',
+    component: 'login-element',
+  },
+  {
+    name: 'login-element', // doet niks impliciet tenzij je het gebruikt
+    path: '/login',
+    component: 'login-element',
+  },
+  {
+    name: '404-catchall', // doet niks impliciet
+    path: '(.*)',           // 404 must be last in this file
+    component: 'not-found',
+  },
 ]);
