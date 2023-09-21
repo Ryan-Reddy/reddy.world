c7f33c645e225db3e562bb01d25e4dfa76a10008
Ryan Reddy <90551370+klugook@users.noreply.github.com>
2023-09-21 10:28:38 +0200

    refactors using a codestyle tool
    Signed-off-by: Ryan Reddy <90551370+klugook@users.noreply.github.com>

--- src/components/global/menu-footer.ts
+++ src/components/global/menu-footer.ts
@@ -1,5 +1,5 @@
-import {css, html, LitElement} from 'lit';
-import {customElement, property, query, queryAll} from 'lit/decorators.js';
+import {css, html, LitElement} from "lit";
+import {customElement, property, query, queryAll} from "lit/decorators.js";
 import langCSS from "../../css/langCSS";
 
 /**
@@ -8,151 +8,155 @@ import langCSS from "../../css/langCSS";
  * @slot - This element has a slot
  * @csspart button - The button
  */
-@customElement('footer-menu')
+@customElement("footer-menu")
 export class MenuFooter extends LitElement {
-  @query('a', true) _input!: HTMLInputElement;
+  @query("a", true) _input!: HTMLInputElement;
   @property() _language: string | undefined;
-  @queryAll('[lang]') _containers!: NodeListOf<HTMLElement>
+  @queryAll("[lang]") _containers!: NodeListOf<HTMLElement>;
 
   constructor() {
     super();
   }
 
   static get styles() {
-    return [langCSS, css`
-      nav {
-        width: 100%;
-        //box-sizing: border-box;
-        text-decoration: none;
-        justify-content: space-between;
-        text-align: center; /* center the child */
-        align-content: center;
-        //float: right;
-        display: flex;
-        align-items: center;
-        margin: .5rem;
-        line-height: 2em; // height of buttons -> stops overlap
-      }
-
-      //.lang-match {
-      //  //width: 100%;
-      //
-      //}
-
-      body {
-        font-family: 'Montserrat', sans-serif;
-        color: white;
-        font-size: 1em;
-        opacity: 1;
-      }
-
-      input {
-        width: 176px;
-        //position: relative;
-        left: 1em;
-        font-family: 'Fira Code', monospace;
-        font-size: 12px;
-        opacity: 1;
-        text-align: left;
-      }
-
-      nav ul {
-        overflow: hidden;
-        align-items: center;
-      }
-
-      nav ul li {
-        display: inline-block; // needed for the spacing on desktop of footer buttons
-        margin: .5rem; // needed for the spacing on desktop of footer buttons
-      }
-
-      nav ul li a { // shape of the buttons in the footer
-        color: var(--wit);
-        border-radius: 8px;
-        text-transform: uppercase;
-        padding: .8rem;
-        text-decoration: none;
-      }
-
-      a.active, a:hover {
-        background: var(--silver);
-        transition: var(--transitionInSeconds);
-        color: var(--zwart);
-      }
-
-      .nav-button {
-        opacity: 1;
-        text-align: center;
-      }
-
-      @media (max-width: 952px) {
+    return [
+      langCSS,
+      css`
         nav {
-          float: none;
-          position: absolute;
+          width: 100%;
+          //box-sizing: border-box;
+          text-decoration: none;
+          justify-content: space-between;
+          text-align: center; /* center the child */
+          align-content: center;
+          //float: right;
+          display: flex;
+          align-items: center;
+          margin: 0.5rem;
+          line-height: 2em; // height of buttons -> stops overlap
         }
 
-        nav ul {
-          margin-left: auto;
-          margin-right: auto;
-          padding-inline-start: 0;
+        //.lang-match {
+        //  //width: 100%;
+        //
+        //}
 
-        }
-
-        nav ul li {
-
-          display: block;
-          margin: .0rem;
-          line-height: 1.5em; // height of buttons -> stops overlap
+        body {
+          font-family: "Montserrat", sans-serif;
+          color: white;
+          font-size: 1em;
+          opacity: 1;
         }
 
         input {
-          max-width: 100%;
-          height: auto;
-          width: auto \\9; /* ie8 */
+          width: 176px;
+          //position: relative;
+          left: 1em;
+          font-family: "Fira Code", monospace;
+          font-size: 12px;
+          opacity: 1;
+          text-align: left;
         }
 
-      }
-
-      @media (max-width: 858px) {
-        display: inline-block;
-
-        .checkbtn {
-          display: inline-block;
+        nav ul {
+          overflow: hidden;
+          align-items: center;
         }
 
-        u {
-          position: fixed;
-          width: auto;
-          height: 100vh;
-          top: 80px;
-          //left: -100%;
-          text-align: center;
-          transition: all .5s;
+        nav li {
+          display: inline-block; // needed for the spacing on desktop of footer buttons
+          margin: 0.5rem; // needed for the spacing on desktop of footer buttons
         }
 
         nav ul li a {
-          font-size: 11vw;
+          // shape of the buttons in the footer
+          color: var(--wit);
+          border-radius: 8px;
+          text-transform: uppercase;
+          padding: 0.8rem;
+          text-decoration: none;
         }
 
-        #check:checked {
-          left: 0;
-        }
-      }
-
-      @media (prefers-color-scheme: light) {
-        footer, nav ul li a {
-          color: var(--zwart);
-          background-color: var(--wit);
-          border-color: var(--wit);
-        }
-
-        a.active, a:hover {
-          background: var(--gold);
+        a.active,
+        a:hover {
+          background: var(--silver);
           transition: var(--transitionInSeconds);
           color: var(--zwart);
         }
-      }
-    `];
+
+        .nav-button {
+          opacity: 1;
+          text-align: center;
+        }
+
+        @media (max-width: 952px) {
+          nav {
+            float: none;
+            position: absolute;
+          }
+
+          nav ul {
+            margin-left: auto;
+            margin-right: auto;
+            padding-inline-start: 0;
+          }
+
+          nav ul li {
+            display: block;
+            margin: 0;
+            line-height: 1.5em; // height of buttons -> stops overlap
+          }
+
+          input {
+            max-width: 100%;
+            height: auto;
+            width: auto \\9; /* ie8 */
+          }
+        }
+
+        @media (max-width: 858px) {
+          display: inline-block;
+
+          .checkbtn {
+            display: inline-block;
+          }
+
+          u {
+            position: fixed;
+            width: auto;
+            height: 100vh;
+            top: 80px;
+            //left: -100%;
+            text-align: center;
+            transition: all 0.5s;
+          }
+
+          nav ul li a {
+            font-size: 11vw;
+          }
+
+          #check:checked {
+            left: 0;
+          }
+        }
+
+        @media (prefers-color-scheme: light) {
+          footer,
+          nav ul li a {
+            color: var(--zwart);
+            background-color: var(--wit);
+            border-color: var(--wit);
+          }
+
+          a.active,
+          a:hover {
+            background: var(--gold);
+            transition: var(--transitionInSeconds);
+            color: var(--zwart);
+          }
+        }
+      `,
+    ];
   }
 
   render() {
@@ -160,76 +164,193 @@ export class MenuFooter extends LitElement {
       <nav>
         <div lang="en">
           <ul>
-            <li><a class="nav-button" href="/ral-contrast-checker" id="contrast-checker-button"
-                   aria-label="art">RAL-contrast</a></li>
-            <li><a class="nav-button" href="/portfolio" id="portfolio-button"
-                   aria-label="art">Portfolio</a></li>
-            <li><a class="nav-button" href="https://github.com/Ryan-Reddy" id="github-button"
-                   aria-label="github">Github</a></li>
-            <li><a class="nav-button" href="https://www.linkedin.com/in/ryan-reddy-productions/" id="linkedin-button"
-                   aria-label="linkedin">Linkedin</a></li>
-            <li><a class="nav-button" data-key="nav-btn-friends" href="friends" id="friends-button"
-                   aria-label="Friends">Friends</a></li>
-            <li><a class="nav-button" data-key="nav-btn-readme" href="readme" id="readme-button" aria-label="Readme">
-              Readme</a>
+            <li>
+              <a
+                class="nav-button"
+                href="/ral-contrast-checker"
+                id="contrast-checker-button"
+                aria-label="art"
+              >RAL-contrast</a
+              >
             </li>
-            <li><a class="nav-button" data-key="nav-btn-friends" href="support" id="support-button"
-                   aria-label="Support">Support</a></li>
-            <li><a class="nav-button" href="contact" id="contact-button" aria-label="Contact">Contact</a>
+            <li>
+              <a
+                class="nav-button"
+                href="/portfolio"
+                id="portfolio-button"
+                aria-label="art"
+              >Portfolio</a
+              >
+            </li>
+            <li>
+              <a
+                class="nav-button"
+                href="https://github.com/Ryan-Reddy"
+                id="github-button"
+                aria-label="github"
+              >Github</a
+              >
+            </li>
+            <li>
+              <a
+                class="nav-button"
+                href="https://www.linkedin.com/in/ryan-reddy-productions/"
+                id="linkedin-button"
+                aria-label="linkedin"
+              >Linkedin</a
+              >
+            </li>
+            <li>
+              <a
+                class="nav-button"
+                data-key="nav-btn-friends"
+                href="friends"
+                id="friends-button"
+                aria-label="Friends"
+              >Friends</a
+              >
+            </li>
+            <li>
+              <a
+                class="nav-button"
+                data-key="nav-btn-readme"
+                href="readme"
+                id="readme-button"
+                aria-label="Readme"
+              >
+                Readme</a
+              >
+            </li>
+            <li>
+              <a
+                class="nav-button"
+                data-key="nav-btn-friends"
+                href="support"
+                id="support-button"
+                aria-label="Support"
+              >Support</a
+              >
+            </li>
+            <li>
+              <a
+                class="nav-button"
+                href="contact"
+                id="contact-button"
+                aria-label="Contact"
+              >Contact</a
+              >
             </li>
           </ul>
         </div>
         <div lang="nl">
           <ul>
-            <li><a class="nav-button" href="/ral-contrast-checker" id="contrast-checker-button"
-                   aria-label="art">RAL-contrast</a></li>
-            <li><a class="nav-button" href="/portfolio" id="portfolio-button"
-                   aria-label="art">Portfolio</a></li>
-            <li><a class="nav-button" href="https://github.com/Ryan-Reddy" id="github-button"
-                   aria-label="github">Github</a></li>
-            <li><a class="nav-button" href="https://www.linkedin.com/in/ryan-reddy-productions/" id="linkedin-button"
-                   aria-label="linkedin">Linkedin</a></li>
-            <li><a class="nav-button" data-key="nav-btn-friends" href="/friends" id="friends-button"
-                   aria-label="Vrienden">Vrienden</a></li>
-            <li><a class="nav-button" data-key="nav-btn-readme" href="/readme" id="readme-button" aria-label="Leesmij">
-              Leesmij</a>
+            <li>
+              <a
+                class="nav-button"
+                href="/ral-contrast-checker"
+                id="contrast-checker-button"
+                aria-label="art"
+              >RAL-contrast</a
+              >
             </li>
-            <li><a class="nav-button" data-key="nav-btn-friends" href="/support" id="support-button"
-                   aria-label="Support">Support</a></li>
-            <li><a class="nav-button" href="${this._contactClickedEnglish}" id="contact-button" aria-label="Contact">
-              Contact</a>
+            <li>
+              <a
+                class="nav-button"
+                href="/portfolio"
+                id="portfolio-button"
+                aria-label="art"
+              >Portfolio</a
+              >
+            </li>
+            <li>
+              <a
+                class="nav-button"
+                href="https://github.com/Ryan-Reddy"
+                id="github-button"
+                aria-label="github"
+              >Github</a
+              >
+            </li>
+            <li>
+              <a
+                class="nav-button"
+                href="https://www.linkedin.com/in/ryan-reddy-productions/"
+                id="linkedin-button"
+                aria-label="linkedin"
+              >Linkedin</a
+              >
+            </li>
+            <li>
+              <a
+                class="nav-button"
+                data-key="nav-btn-friends"
+                href="/friends"
+                id="friends-button"
+                aria-label="Vrienden"
+              >Vrienden</a
+              >
+            </li>
+            <li>
+              <a
+                class="nav-button"
+                data-key="nav-btn-readme"
+                href="/readme"
+                id="readme-button"
+                aria-label="Leesmij"
+              >
+                Leesmij</a
+              >
+            </li>
+            <li>
+              <a
+                class="nav-button"
+                data-key="nav-btn-friends"
+                href="/support"
+                id="support-button"
+                aria-label="Support"
+              >Support</a
+              >
+            </li>
+            <li>
+              <a
+                class="nav-button"
+                href="${this._contactClickedEnglish}"
+                id="contact-button"
+                aria-label="Contact"
+              >
+                Contact</a
+              >
             </li>
           </ul>
         </div>
-
       </nav>
     `;
   }
 
   connectedCallback() {
-    super.connectedCallback()
-    this._setLanguage()
+    super.connectedCallback();
+    this._setLanguage();
   }
 
   firstUpdated() {
-    console.log(this._containers)
-    this._setLanguage()
-    this._containers.forEach(value => {
+    console.log(this._containers);
+    this._setLanguage();
+    this._containers.forEach((value) => {
       if (value.lang == this._language) {
-        value.className = "lang-match"
-        console.log(value)
+        value.className = "lang-match";
+        console.log(value);
       }
-    })
+    });
   }
 
   _setLanguage() {
     this._language = navigator.language;
-    console.log(this._language)
-    this._language = this._language.split('-')[0]
-    if (this._language != 'nl' || 'en') {
-      this._language = 'en'; // set default english if no other lang found
+    console.log(this._language);
+    this._language = this._language.split("-")[0];
+    if (this._language != "nl" || "en") {
+      this._language = "en"; // set default english if no other lang found
     }
-    console.log(this._language)
+    console.log(this._language);
   }
 
   protected createRenderRoot() {
