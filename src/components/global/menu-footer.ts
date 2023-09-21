6342efca8c81365c903f19778872ada197449a88
Ryan Reddy <90551370+klugook@users.noreply.github.com>
2023-09-21 10:22:43 +0200

    - fixes footer menu button overlap vertically
    Signed-off-by: Ryan Reddy <90551370+klugook@users.noreply.github.com>

--- src/components/global/menu-footer.ts
+++ src/components/global/menu-footer.ts
@@ -17,156 +17,142 @@ export class MenuFooter extends LitElement {
   constructor() {
     super();
   }
+
   static get styles() {
-    return [
-      langCSS,
-      css`
+    return [langCSS, css`
+      nav {
+        width: 100%;
+        //box-sizing: border-box;
+        text-decoration: none;
+        justify-content: space-between;
+        text-align: center; /* center the child */
+        align-content: center;
+        //float: right;
+        display: flex;
+        align-items: center;
+        margin: .5rem;
+        line-height: 2em; // height of buttons -> stops overlap
+      }
+
+      //.lang-match {
+      //  //width: 100%;
+      //
+      //}
+
+      body {
+        font-family: 'Montserrat', sans-serif;
+        color: white;
+        font-size: 1em;
+        opacity: 1;
+      }
+
+      input {
+        width: 176px;
+        //position: relative;
+        left: 1em;
+        font-family: 'Fira Code', monospace;
+        font-size: 12px;
+        opacity: 1;
+        text-align: left;
+      }
+
+      nav ul {
+        overflow: hidden;
+        align-items: center;
+      }
+
+      nav ul li {
+        display: inline-block; // needed for the spacing on desktop of footer buttons
+        margin: .5rem; // needed for the spacing on desktop of footer buttons
+      }
+
+      nav ul li a { // shape of the buttons in the footer
+        color: var(--wit);
+        border-radius: 8px;
+        text-transform: uppercase;
+        padding: .8rem;
+        text-decoration: none;
+      }
+
+      a.active, a:hover {
+        background: var(--silver);
+        transition: var(--transitionInSeconds);
+        color: var(--zwart);
+      }
+
+      .nav-button {
+        opacity: 1;
+        text-align: center;
+      }
+
+      @media (max-width: 952px) {
         nav {
-          width: 100%;
-
-          box-sizing: border-box;
-          text-decoration: none;
-          justify-content: space-between;
-          text-align: center; /* center the child */
-          align-content: center;
-
-
-          //float: right;
-
-          display: flex;
-          align-items: center;
-
-          margin: .5rem;
-          line-height: 1.5em;
-        }
-
-        .lang-match {
-          width: 100%;
-
-        }
-
-        body {
-          font-family: Montserrat;
-          color: white;
-          font-size: 1em;
-          opacity: 1;
-        }
-
-        input {
-          width: 176px;
-          position: relative;
-          left: 1em;
-          font-family: Fira Code;
-          font-size: 12px;
-          opacity: 1;
-          text-align: left;
-
+          float: none;
+          position: absolute;
         }
 
         nav ul {
-          overflow: hidden;
-          align-items: center;
+          margin-left: auto;
+          margin-right: auto;
+          padding-inline-start: 0;
 
         }
 
         nav ul li {
-          display: inline-block;
-          margin: .5rem;
 
+          display: block;
+          margin: .0rem;
+          line-height: 1.5em; // height of buttons -> stops overlap
+        }
+
+        input {
+          max-width: 100%;
+          height: auto;
+          width: auto \\9; /* ie8 */
+        }
+
+      }
+
+      @media (max-width: 858px) {
+        display: inline-block;
+
+        .checkbtn {
+          display: inline-block;
+        }
+
+        u {
+          position: fixed;
+          width: auto;
+          height: 100vh;
+          top: 80px;
+          //left: -100%;
+          text-align: center;
+          transition: all .5s;
         }
 
         nav ul li a {
-          color: var(--wit);
-          border-radius: 8px;
-          text-transform: uppercase;
-          padding: .8rem;
-          text-decoration: none;
+          font-size: 11vw;
+        }
 
+        #check:checked {
+          left: 0;
+        }
+      }
+
+      @media (prefers-color-scheme: light) {
+        footer, nav ul li a {
+          color: var(--zwart);
+          background-color: var(--wit);
+          border-color: var(--wit);
         }
 
         a.active, a:hover {
-          background: var(--silver);
+          background: var(--gold);
           transition: var(--transitionInSeconds);
           color: var(--zwart);
         }
-
-        .nav-button {
-          opacity: 1;
-          text-align: center;
-        }
-
-        @media (max-width: 952px) {
-          nav {
-            //float: none;
-            position: absolute;
-
-
-          }
-
-          nav ul {
-            margin-left: auto;
-            margin-right: auto;
-            padding-inline-start: 0;
-
-          }
-
-          nav ul li {
-
-            display: block;
-            margin: .0rem;
-
-            line-height: 1.5rem;
-          }
-
-          input {
-            max-width: 100%;
-            height: auto;
-            width: auto \\9; /* ie8 */
-          }
-
-        }
-
-        @media (max-width: 858px) {
-          display: inline-block;
-
-          .checkbtn {
-            display: inline-block;
-          }
-
-          u {
-            position: fixed;
-            width: auto;
-            height: 100vh;
-            top: 80px;
-            //left: -100%;
-            text-align: center;
-            transition: all .5s;
-          }
-
-          nav ul li a {
-            font-size: 11vw;
-          }
-
-          #check:checked {
-            left: 0;
-          }
-        }
-
-        @media (prefers-color-scheme: light) {
-          footer, nav ul li a {
-            color: var(--zwart);
-            background-color: var(--wit);
-            border-color: var(--wit);
-          }
-
-          a.active, a:hover {
-            background: var(--gold);
-            transition: var(--transitionInSeconds);
-            color: var(--zwart);
-          }
-        }
-      `
-    ];
+      }
+    `];
   }
 
   render() {
@@ -184,7 +170,8 @@ export class MenuFooter extends LitElement {
                    aria-label="linkedin">Linkedin</a></li>
             <li><a class="nav-button" data-key="nav-btn-friends" href="friends" id="friends-button"
                    aria-label="Friends">Friends</a></li>
-            <li><a class="nav-button" data-key="nav-btn-readme" href="readme" id="readme-button" aria-label="Readme">Readme</a>
+            <li><a class="nav-button" data-key="nav-btn-readme" href="readme" id="readme-button" aria-label="Readme">
+              Readme</a>
             </li>
             <li><a class="nav-button" data-key="nav-btn-friends" href="support" id="support-button"
                    aria-label="Support">Support</a></li>
@@ -204,11 +191,13 @@ export class MenuFooter extends LitElement {
                    aria-label="linkedin">Linkedin</a></li>
             <li><a class="nav-button" data-key="nav-btn-friends" href="/friends" id="friends-button"
                    aria-label="Vrienden">Vrienden</a></li>
-            <li><a class="nav-button" data-key="nav-btn-readme" href="/readme" id="readme-button" aria-label="Leesmij">Leesmij</a>
+            <li><a class="nav-button" data-key="nav-btn-readme" href="/readme" id="readme-button" aria-label="Leesmij">
+              Leesmij</a>
             </li>
             <li><a class="nav-button" data-key="nav-btn-friends" href="/support" id="support-button"
                    aria-label="Support">Support</a></li>
-            <li><a class="nav-button" href="${this._contactClickedEnglish}" id="contact-button" aria-label="Contact">Contact</a>
+            <li><a class="nav-button" href="${this._contactClickedEnglish}" id="contact-button" aria-label="Contact">
+              Contact</a>
             </li>
           </ul>
         </div>
@@ -221,9 +210,7 @@ export class MenuFooter extends LitElement {
     super.connectedCallback()
     this._setLanguage()
   }
-  protected createRenderRoot() {
-    return super.createRenderRoot();
-  }
+
   firstUpdated() {
     console.log(this._containers)
     this._setLanguage()
@@ -237,11 +224,15 @@ export class MenuFooter extends LitElement {
 
   _setLanguage() {
     this._language = navigator.language;
-      console.log(this._language)
-      this._language = this._language.split('-')[0]
-    if (this._language != 'nl' || 'en' ) {
+    console.log(this._language)
+    this._language = this._language.split('-')[0]
+    if (this._language != 'nl' || 'en') {
       this._language = 'en'; // set default english if no other lang found
     }
     console.log(this._language)
+  }
+
+  protected createRenderRoot() {
+    return super.createRenderRoot();
   }
 }
