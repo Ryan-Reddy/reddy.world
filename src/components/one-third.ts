75f8e278dce529f09dd0d28824ae83e03cd71ec8
Ryan Reddy <90551370+klugook@users.noreply.github.com>
2023-09-22 05:38:21 +0200

    - improves the cosmic celebration page
    Signed-off-by: Ryan Reddy <90551370+klugook@users.noreply.github.com>

--- src/components/one-third.ts
+++ src/components/one-third.ts
@@ -2,12 +2,6 @@ import {css, html, LitElement} from 'lit';
 import {customElement, property, query} from 'lit/decorators.js';
 import '/css/mainCSS.css';
 
-/**
- * An example element.
- *
- * @slot - This element has a slot
- * @csspart button - The button
- */
 @customElement('one-third-element')
 export class ContactElement extends LitElement {
   @property() _hiddenElement = 'hidden';
@@ -24,92 +18,67 @@ export class ContactElement extends LitElement {
 
   static get styles() {
     return css`
-      * {
-        margin: 0;
-        padding: 0;
-        box-sizing: border-box;
-        text-decoration: none;
-      }
 
-      h1 {
-        padding-top: 20vh;
-      }
-
-      h1, h2, h3 {
-        align-items: center;
-        text-align: center;
-        margin-right: auto;
-        margin-left: auto;
-        margin-top: auto;
-      }
 
       .paragraph {
-        padding-top: 20vh;
-        padding-bottom: 20vh;
+        margin-top: 20vh;
+        margin-bottom: 20vh;
       }
 
-      p {
-        align-items: center;
+      .invitation {
+        background-color: var(--background-color);
+        padding: 20px;
+        border-radius: 10px;
+        box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
         text-align: center;
-        margin-right: auto;
-        margin-left: auto;
-        margin-top: auto;
+        margin-bottom: 20px;
       }
 
-      .hidden {
-        display: none;
-        pointer-events: none;
-        color: var(--grijs);
-        background-color: var(--grijs);
+      .invitation h2 {
+        font-size: 24px;
+        color: var(--primary-color);
       }
 
-      ul {
-        list-style: none;
-        width: 100%
-
+      .invitation p {
+        font-size: 18px;
+        margin-top: 10px;
       }
 
-      input, textarea, button {
-        padding: 5px;
-        width: 100%;
-        margin: 12px;
-        height: 32px;
+      .invitation a.button {
+        text-decoration: none;
+        display: inline-block;
+        background: var(--primary-color);
+        color: white;
+        font-size: 18px;
+        border: none;
+        border-radius: 5px;
+        padding: 10px 20px;
+        cursor: pointer;
+        transition: background 0.3s ease;
       }
 
-      textarea {
-        height: 256px;
+      .invitation a.button:hover {
+        background: var(--secondary-color);
+        height: 2rem;
       }
-
-      #submitBtn {
-        background: rgba(250, 235, 215, 0.7);
-        color: rgba(21, 23, 21, 1);
-      }
-
-    ,
-    css \`
-        /* Include your external CSS file here */
-        @import url('./path-to-external.css'); /* Replace 'path-to-external.css' with the actual path to your CSS file */
-         \`,
-
     `;
   }
 
   firstUpdated(changedProperties: any) {
     let titleEvent = new CustomEvent('title-change', {
       detail: {
-        message: 'Contact'
-      }
+        message: 'Contact',
+      },
     });
-    console.log('dispatching event:' + titleEvent.detail.message)
+    console.log('dispatching event:' + titleEvent.detail.message);
     this.dispatchEvent(titleEvent);
 
     // Wait for DOMContentLoaded event to ensure the DOM is ready
     window.addEventListener('DOMContentLoaded', () => {
       // Log a message to verify if this code is executed
-      console.log('DOMContentLoaded event fired');
 
       // Scroll down 200 pixels from the top
-      window.scrollTo(0, 0)
+      window.scrollTo(0, 0);
       window.scrollBy(0, 200);
     });
   }
@@ -125,27 +94,24 @@ export class ContactElement extends LitElement {
       <meta title="one third ">
       <body>
       <main>
-        <h1>A Cosmic Celebration</h1>
+        <div class="invitation">
+          <h1>A Cosmic Celebration</h1>
+          <div class="paragraph">
+            <h2>Join me for a gathering beneath the starlit tapestry.</h2>
+            <p>As the clock ticks toward my 🌌 <i>33,333rd</i> orbit, we'll come together for a celestial soirée.</p>
+            <p>Save the date: <i>December 24, 2024</i>, from <i>3:00 PM to 6:00 PM</i>.</p>
+            <p>A celestial moment etched in the heavens. 🌠🌌</p>
+            <p>* Born amidst the stars on <i>21-09-1991</i>.</p>
 
-        <div class="paragraph">
-          <h2>Join me for a clandestine gathering beneath the starlit tapestry.</h2>
-          <br>
-          <p>As the clock ticks toward my 🌌 <i>33,333rd</i> orbit,</p>
-          <br>
+            <a href="mailto:ryan@reddy.world?subject=RSVP for Cosmic Celebration" class="button">RSVP Now</a>
 
-          <p> we'll gather for a cosmic little celebration. </p>
-          <p>Save the date: <i>December 24, 2024</i>,
-            <br>
+            <h3>Stay tuned for more details in due course.</h3>
+          </div>
+        </div>
 
-            a celestial moment marked in the heavens. 🌠🌌</p>
-          <br>
-          <p>* Born amidst the stars on <i>21-09-1991</i></p>
-
-          <br>
-          <br>
-          <h3>More t.b.a. in due course.</h3>
 
         </div>
+
 
       </main>
       </body>
